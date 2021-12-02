daa9b5dd8bdefd0b4406ad4f1db3ce2bb0aa7a95
kakaacat <1912445179@qq.com>
2021-12-02 15:04:02 +0800

    LeetCode_Study topic16(第K大的数)

--- src/main/java/com/kaka/LC11_20/topic16/KthLargestElementInAnArray.java
+++ src/main/java/com/kaka/LC11_20/topic16/KthLargestElementInAnArray.java
@@ -82,4 +82,51 @@ public class KthLargestElementInAnArray {
         nums[r] = nums[l];
         nums[l] = temp;
     }
+
+    /**
+     * 3、堆排序(大根堆)
+     */
+    public int findKthLargest3(int nums[], int k){
+        int heapSize = nums.length;
+        buildMaxHeap(nums, heapSize);
+        //建堆完毕后，nums【0】为最大元素。逐个删除堆顶元素，直到删除了k-1个。
+        for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
+            //先将堆的最后一个元素与堆顶元素交换，由于此时堆的性质被破坏，需对此时的根节点进行向下调整操作。
+            swap(nums, i, 0);
+            --heapSize;     //相当于删除了最大值
+            maxHeapify(nums, 0, heapSize);  //重新排序找出最大值
+        }
+
+        return nums[0];
+    }
+
+    //建立一个大根堆
+    private void buildMaxHeap(int[] nums, int heapSize){
+        //从最后一个父节点位置开始调整每一个节点的子树。
+        // 数组长度为heapsize，因此最后一个节点的位置为heapsize-1，所以父节点的位置为heapsize-1-1/2。
+        for (int i = (heapSize-2)/ 2; i >= 0; --i) {
+            maxHeapify(nums, i, heapSize);
+        }
+    }
+    //调整当前结点和子节点的顺序。i:需要调整的节点
+    public void maxHeapify(int[] nums, int i, int heapSize) {
+        //left和right表示当前父节点i的两个左右子节点。
+        int left = i * 2 + 1;
+        int right = i * 2 + 2;
+        int largest = i;    //最大值指针
+        //如果左子点在数组内，且比当前父节点大，则将最大值的指针指向左子点。
+        if(left < heapSize && nums[left] > nums[largest]){
+            largest = left;
+        }
+        //如果右子点在数组内，且比当前父节点大，则将最大值的指针指向右子点。
+        if(right < heapSize && nums[right] > nums[largest]){
+            largest = right;
+        }
+        //如果最大值的指针不是父节点，则交换父节点和当前最大值指针指向的子节点。
+        if(largest != i){
+            swap(nums, i, largest);
+            //由于交换了父节点和子节点，因此可能对子节点的子树造成影响，所以对子节点的子树进行调整。
+            maxHeapify(nums, largest, heapSize);
+        }
+    }
 }
