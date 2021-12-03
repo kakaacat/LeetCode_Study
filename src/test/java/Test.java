a1dcbb8665174fc52dc19d85e9cb55c0046e54d5
kakaacat <1912445179@qq.com>
2021-12-03 10:28:45 +0800

    LeetCode_Study topic17(K 个一组翻转链表-Test)

---
+++ src/test/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Author : kaka
+ * @Date: 2021-12-03 10:26
+ */
+public class Test {
+    @org.junit.Test
+    public void mytest(){
+        System.out.println("POPO");
+        System.out.println("POPO");
+    }
+}
