2b18fe0c61f7e5e334c8fe618f689a85c276f8d6
kakaacat <1912445179@qq.com>
2021-12-16 10:31:06 +0800

    LeetCode_Study topic30(字符串相加)

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,5 @@
 import com.kaka.LC11_20.topic18.SortAnArray;
+import com.kaka.LC21_30.topic30.AddStrings;
 
 /**
  * @Author : kaka
@@ -7,5 +8,11 @@ import com.kaka.LC11_20.topic18.SortAnArray;
 public class Test {
     @org.junit.Test
     public void mytest(){
+        String num1 = "1";
+        String num2 = "9";
+        AddStrings addStrings = new AddStrings();
+        String s = addStrings.addStrings(num1, num2);
+        System.out.println(s);
+
     }
 }
