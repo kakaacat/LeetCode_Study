1c42b5ba06ecfc617a23a678f13bc3e8260906fc
kakaacat <1912445179@qq.com>
2021-12-26 16:58:17 +0800

    LeetCode_Study topic40(接雨水1+2)

--- src/main/java/com/kaka/LC31_40/topic40/TrappingRainWater.java
+++ src/main/java/com/kaka/LC31_40/topic40/TrappingRainWater.java
@@ -39,4 +39,44 @@ public class TrappingRainWater {
         }
         return ans;
     }
+
+    /**
+     * 2.动态编程
+     * 在暴力方法中，我们仅仅为了找到最大值每次都要向左和向右扫描一次。
+     * 但是我们可以提前存储这个值。因此，可以通过动态编程解决。
+     *
+     * 时间复杂度：O(n)。
+     * 存储最大高度数组，需要两次遍历，每次 O(n) 。
+     * 最终使用存储的数据更新ans ，O(n)。
+     * 空间复杂度：O(n) 额外空间。
+     * 和方法 1 相比使用了额外的 O(n) 空间用来放置 left_max 和 right_max 数组。
+     */
+
+    public int trap2(int[] height){
+        if (height == null || height.length == 0){
+            return 0;
+        }
+        int ans = 0;
+        int size = height.length;
+
+        int[] maxLeft = new int[size];
+        int[] maxRight = new int[size];
+
+        //存储左边最大值
+        maxLeft[0] = height[0];
+        for (int i = 1; i < size; i++) {
+            maxLeft[i] = Math.max(maxLeft[i- 1], height[i]);
+        }
+        //存储右边最大值
+        maxRight[size - 1] = height[size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            maxRight[i] = Math.max(maxRight[i + 1], height[i]);
+        }
+
+        for (int i = 1; i < size - 1; i++) {
+            ans += Math.min(maxLeft[i], maxRight[i]) - height[i];
+        }
+
+        return ans;
+    }
 }
