c845761d93a963baeb34f07ae82c0a92221ce3b5
kakaacat <1912445179@qq.com>
2021-12-27 11:39:52 +0800

    LeetCode_Study topic40(二叉树的中序遍历1)

---
+++ src/main/java/com/kaka/LC41_50/topic41/BinaryTreeInorderTraversal.java
@@ -0,0 +1,40 @@
+package com.kaka.LC41_50.topic41;
+
+import com.kaka.LC21_30.topic27.TreeNode;
+
+import java.util.*;
+
+/**
+ * @Author : kaka
+ * @Date: 2021-12-27 11:18
+ *
+ * @Description: 二叉树的中序遍历
+ *
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ */
+public class BinaryTreeInorderTraversal {
+    /**
+     * 1.递归
+     *
+     * 时间复杂度：O(n)，其中 n 为二叉树节点的个数。二叉树的遍历中每个节点会被访问一次且只会被访问一次。
+     * 空间复杂度：O(n)。空间复杂度取决于递归的栈深度，而栈深度在二叉树为一条链的情况下会达到 O(n) 的级别。
+     */
+    public List<Integer> inorderTraversal1(TreeNode root){
+        List<Integer> ans = new ArrayList<>();
+        inorder(root, ans);
+        return ans;
+    }
+
+    private void inorder(TreeNode root, List<Integer> ans) {
+        if (root == null){
+            return;
+        }
+        inorder(root.left, ans);
+        ans.add(root.val);
+        inorder(root.right, ans);
+    }
+
+
+}
