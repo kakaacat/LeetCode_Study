6ca800e8962acd16c1d56766a119a556d0d6b72e
kakaacat <1912445179@qq.com>
2022-01-06 17:38:54 +0800

    LeetCode_Study topic51(82. 删除排序链表中的重复元素 II)

--- src/main/java/com/kaka/LC51_60/topic51/RemoveDuplicatesFromSortedListII.java
+++ src/main/java/com/kaka/LC51_60/topic51/RemoveDuplicatesFromSortedListII.java
@@ -1,8 +1,13 @@
 package com.kaka.LC51_60.topic51;
 
+import com.kaka.LC1_10.day02.ListNode;
+
+import java.awt.geom.CubicCurve2D;
+import java.util.List;
+
 /**
  * @Author : kaka
- * @Date: 2022-01-05 16:38
+ * @Date: 2022-01-06 16:38
  *
  * @Description: 82. 删除排序链表中的重复元素 II
  *
@@ -13,4 +18,34 @@ package com.kaka.LC51_60.topic51;
  * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii
  */
 public class RemoveDuplicatesFromSortedListII {
+    /**
+     * 1.一次遍历
+     *
+     * 时间复杂度：O(n)，其中 n 是链表的长度。
+     * 空间复杂度：O(1)。
+     */
+    public ListNode deleteDuplicates(ListNode head){
+        if (head == null){
+            return head;
+        }
+
+        ListNode preHead = new ListNode(-101, head);
+        ListNode prev = preHead;
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null){
+            if (curr.val == curr.next.val){
+                int val = curr.val;
+                while (curr != null && curr.val == val){
+                    curr = curr.next;
+                }
+                prev.next = curr;
+            } else {
+               prev = curr;
+               curr = curr.next;
+            }
+        }
+
+        return preHead.next;
+    }
 }
