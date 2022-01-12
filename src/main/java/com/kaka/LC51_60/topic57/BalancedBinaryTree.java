32ea7d7e21df5477311bb9543753d3343d85e0cf
kakaacat <1912445179@qq.com>
2022-01-12 19:26:44 +0800

    LeetCode_Study topic57(110.平衡二叉树)

---
+++ src/main/java/com/kaka/LC51_60/topic57/BalancedBinaryTree.java
@@ -0,0 +1,21 @@
+package com.kaka.LC51_60.topic57;
+
+import com.kaka.LC21_30.topic27.TreeNode;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-01-12 19:24
+ *
+ * @Description: 110. 平衡二叉树
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ *
+ * https://leetcode-cn.com/problems/balanced-binary-tree/
+ */
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root){
+        return true;
+    }
+}
