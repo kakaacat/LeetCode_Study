f5e93e851641b49a12b460a989e9dc5e22be8159
kakaacat <1912445179@qq.com>
2022-01-15 12:18:47 +0800

    LeetCode_Study topic57(110.平衡二叉树)

--- src/main/java/com/kaka/LC51_60/topic57/BalancedBinaryTree.java
+++ src/main/java/com/kaka/LC51_60/topic57/BalancedBinaryTree.java
@@ -16,7 +16,21 @@ import com.kaka.LC21_30.topic27.TreeNode;
  */
 public class BalancedBinaryTree {
     public boolean isBalanced(TreeNode root){
-        System.out.println();
-        return true;
+        return height(root) >= 0;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1){
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+
     }
 }
