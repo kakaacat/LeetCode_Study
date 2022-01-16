fddcd265f80b790a4b70b843323d0b4e062a3294
kakaacat <1912445179@qq.com>
2022-01-16 20:31:51 +0800

    LeetCode_Study topic58(104. 二叉树的最大深度1+2)

--- src/main/java/com/kaka/LC51_60/topic58/MaxDepthOfBinaryTree.java
+++ src/main/java/com/kaka/LC51_60/topic58/MaxDepthOfBinaryTree.java
@@ -2,6 +2,9 @@ package com.kaka.LC51_60.topic58;
 
 import com.kaka.LC21_30.topic27.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @Author : kaka
  * @Date: 2022-01-16 20:15
@@ -17,6 +20,10 @@ import com.kaka.LC21_30.topic27.TreeNode;
 public class MaxDepthOfBinaryTree {
     /**
      * dfs
+     *
+     * 时间复杂度：O(n)，其中 n 为二叉树节点的个数。每个节点在递归中只被遍历一次。
+     * 空间复杂度：O(height)，其中 height 表示二叉树的高度。递归函数需要栈空间，
+     * 而栈空间取决于递归的深度，因此空间复杂度等价于二叉树的高度。
      */
     public int maxDepth(TreeNode root){
         if (root == null){
@@ -29,6 +36,32 @@ public class MaxDepthOfBinaryTree {
     }
 
     /**
-     * 
+     * bfs
+     *
+     * 时间复杂度：O(n)，其中 n 为二叉树的节点个数。与方法一同样的分析，每个节点只会被访问一次。
+     * 空间复杂度：此方法空间的消耗取决于队列存储的元素数量，其在最坏情况下会达到 O(n)。
      */
+    public int maxDepth2(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+            ans++;
+        }
+
+        return ans;
+    }
 }
