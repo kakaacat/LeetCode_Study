28d7df6ebb53e8c26ceb853dce0ed3e0e1ed9d7b
kakaacat <1912445179@qq.com>
2022-01-18 21:07:06 +0800

    LeetCode_Study topic60(31.下一个排列)

---
+++ src/main/java/com/kaka/LC51_60/topic60/NextPermutation.java
@@ -0,0 +1,19 @@
+package com.kaka.LC51_60.topic60;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-01-18 21:05
+ *
+ * @Description: 31. 下一个排列
+ *
+ * 实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 必须 原地 修改，只允许使用额外常数空间。
+ *
+ * 链接：https://leetcode-cn.com/problems/next-permutation
+ */
+public class NextPermutation {
+    public void nextPermutation(int[] nums){
+
+    }
+}
