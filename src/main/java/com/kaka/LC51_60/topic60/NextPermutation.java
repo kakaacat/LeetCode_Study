58fa392eb6cfb77e093e74feb843f60c83d5e12a
kakaacat <1912445179@qq.com>
2022-01-22 11:18:29 +0800

    LeetCode_Study topic60(31.下一个排列)

--- src/main/java/com/kaka/LC51_60/topic60/NextPermutation.java
+++ src/main/java/com/kaka/LC51_60/topic60/NextPermutation.java
@@ -14,6 +14,34 @@ package com.kaka.LC51_60.topic60;
  */
 public class NextPermutation {
     public void nextPermutation(int[] nums){
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        if (i >= 0){
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]){
+                j--;
+            }
 
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int i) {
+        int start = i, end = nums.length - 1;
+        while (start < end){
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp;
+        temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
