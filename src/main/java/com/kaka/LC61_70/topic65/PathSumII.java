9fec6857e5153192a50bc04f9141258c817a43a3
kakaacat <1912445179@qq.com>
2022-02-03 16:11:28 +0800

    LeetCode_Study topic60(113. 路径总和 II)

--- src/main/java/com/kaka/LC61_70/topic65/PathSumII.java
+++ src/main/java/com/kaka/LC61_70/topic65/PathSumII.java
@@ -1,7 +1,10 @@
 package com.kaka.LC61_70.topic65;
 
 import com.kaka.LC21_30.topic27.TreeNode;
+import javafx.scene.transform.Rotate;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,7 +19,33 @@ import java.util.List;
  * 链接：https://leetcode-cn.com/problems/path-sum-ii
  */
 public class PathSumII {
+    /**
+     * dfs
+     *
+     * 时间复杂度：O(N^2)
+     * 空间复杂度：O(N)
+     */
+    List<List<Integer>> ret = new LinkedList<List<Integer>>();
+    Deque<Integer> path = new LinkedList<Integer>();
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, targetSum);
+        return ret;
+    }
 
+    private void dfs(TreeNode root, int targetSum) {
+        if (root == null){
+            return;
+        }
+        path.offerLast(root.val);
+        targetSum -= root.val;
+
+        if (root.left == null && root.right == null && targetSum == 0){
+            ret.add(new LinkedList<Integer>(path));
+        }
+
+        dfs(root.left, targetSum);
+        dfs(root.right, targetSum);
+
+        path.pollLast();
     }
 }
