ac31183f5db397585f8e3250040e5770950102c7
kakaacat <1912445179@qq.com>
2022-02-03 16:13:49 +0800

    LeetCode_Study topic60(113. 路径总和 II)

--- src/main/java/com/kaka/LC61_70/topic65/PathSumII.java
+++ src/main/java/com/kaka/LC61_70/topic65/PathSumII.java
@@ -3,9 +3,7 @@ package com.kaka.LC61_70.topic65;
 import com.kaka.LC21_30.topic27.TreeNode;
 import javafx.scene.transform.Rotate;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author : kaka
@@ -47,5 +45,56 @@ public class PathSumII {
         dfs(root.right, targetSum);
 
         path.pollLast();
+    }
+
+    /**
+     * bfs
+     */
+//    List<List<Integer>> ret = new LinkedList<List<Integer>>();
+    Map<TreeNode, TreeNode> map = new HashMap<TreeNode, TreeNode>();
+
+    public List<List<Integer>> pathSum1(TreeNode root, int targetSum) {
+        if (root == null) {
+            return ret;
+        }
+
+        Queue<TreeNode> queueNode = new LinkedList<TreeNode>();
+        Queue<Integer> queueSum = new LinkedList<Integer>();
+        queueNode.offer(root);
+        queueSum.offer(0);
+
+        while (!queueNode.isEmpty()) {
+            TreeNode node = queueNode.poll();
+            int rec = queueSum.poll() + node.val;
+
+            if (node.left == null && node.right == null) {
+                if (rec == targetSum) {
+                    getPath(node);
+                }
+            } else {
+                if (node.left != null) {
+                    map.put(node.left, node);
+                    queueNode.offer(node.left);
+                    queueSum.offer(rec);
+                }
+                if (node.right != null) {
+                    map.put(node.right, node);
+                    queueNode.offer(node.right);
+                    queueSum.offer(rec);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public void getPath(TreeNode node) {
+        List<Integer> temp = new LinkedList<Integer>();
+        while (node != null) {
+            temp.add(node.val);
+            node = map.get(node);
+        }
+        Collections.reverse(temp);
+        ret.add(new LinkedList<Integer>(temp));
     }
 }
