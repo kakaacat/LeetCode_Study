5c71b67c08d8b2a8ce0268d5352bfd8267f91767
kakaacat <1912445179@qq.com>
2022-02-05 15:25:35 +0800

    LeetCode_Study topic60(41. 缺失的第一个正数)

--- src/main/java/com/kaka/LC61_70/topic66/FilstMissingPositive.java
+++ src/main/java/com/kaka/LC61_70/topic66/FilstMissingPositive.java
@@ -46,6 +46,20 @@ public class FilstMissingPositive {
      * 置换
      */
     public int firstMissingPositive2(int[] nums){
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1){
+                return i + 1;
+            }
+        }
 
+        return n + 1;
     }
 }
