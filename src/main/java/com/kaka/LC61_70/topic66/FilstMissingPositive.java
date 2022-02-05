f0f539c68c936b9c472057695c3b2b716eefde65
kakaacat <1912445179@qq.com>
2022-02-05 15:10:29 +0800

    LeetCode_Study topic60(41. 缺失的第一个正数)

---
+++ src/main/java/com/kaka/LC61_70/topic66/FilstMissingPositive.java
@@ -0,0 +1,21 @@
+package com.kaka.LC61_70.topic66;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-02-05 15:08
+ *
+ * @Description: 41. 缺失的第一个正数
+ *
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ *
+ * 链接：https://leetcode-cn.com/problems/first-missing-positive
+ */
+public class FilstMissingPositive {
+    /**
+     * 哈希表
+     */
+   public int filstMissingPositive(int[] nums){
+
+   }
+}
