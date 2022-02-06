e5e904b4d3f4f03f11f91c6407dbe20d6e686288
kakaacat <1912445179@qq.com>
2022-02-06 13:32:43 +0800

    LeetCode_Study topic60(22. 括号生成)

--- src/main/java/com/kaka/LC61_70/topic67/GenerateParentheses.java
+++ src/main/java/com/kaka/LC61_70/topic67/GenerateParentheses.java
@@ -1,5 +1,6 @@
 package com.kaka.LC61_70.topic67;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,7 +18,29 @@ import java.util.List;
  * 链接：https://leetcode-cn.com/problems/generate-parentheses
  */
 public class GenerateParentheses {
+    /**
+     * 回溯
+     */
     public List<String> generateParethesis(int n){
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
 
+    private void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2){
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max){
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open){
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
     }
 }
