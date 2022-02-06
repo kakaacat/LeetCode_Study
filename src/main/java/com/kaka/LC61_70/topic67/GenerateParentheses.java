9dd3ad2f28cac10832c93c4998d79efd18698c69
kakaacat <1912445179@qq.com>
2022-02-06 11:59:49 +0800

    LeetCode_Study topic60(22. 括号生成)

---
+++ src/main/java/com/kaka/LC61_70/topic67/GenerateParentheses.java
@@ -0,0 +1,23 @@
+package com.kaka.LC61_70.topic67;
+
+import java.util.List;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-02-06 11:58
+ *
+ * @Description: 22. 括号生成
+ *
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+ * 示例 1：
+ * 输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ *
+ * 链接：https://leetcode-cn.com/problems/generate-parentheses
+ */
+public class GenerateParentheses {
+    public List<String> generateParethesis(int n){
+
+    }
+}
