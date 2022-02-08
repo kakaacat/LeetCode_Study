690b18fec6f3977fc731274b5c418736addd9a02
kakaacat <1912445179@qq.com>
2022-02-08 18:13:14 +0800

    LeetCode_Study(543. 二叉树的直径)

---
+++ src/main/java/com/kaka/LC61_70/topic69/DiameterOfBinatryTree.java
@@ -0,0 +1,20 @@
+package com.kaka.LC61_70.topic69;
+
+import com.kaka.LC21_30.topic27.TreeNode;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-02-08 18:11
+ *
+ * @Description: 543. 二叉树的直径
+ *
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。
+ * 这条路径可能穿过也可能不穿过根结点。
+ *
+ * https://leetcode-cn.com/problems/diameter-of-binary-tree/
+ */
+public class DiameterOfBinatryTree {
+    public int diameterOfBinatryTree(TreeNode root){
+
+    }
+}
