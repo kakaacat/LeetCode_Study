543f48685f7be395f65a9e37c6db5e0aa42459c8
kakaacat <1912445179@qq.com>
2022-02-09 18:52:26 +0800

    LeetCode_Study(98. 验证二叉搜索树)

---
+++ src/main/java/com/kaka/LC61_70/topic70/ValidBTree.java
@@ -0,0 +1,20 @@
+package com.kaka.LC61_70.topic70;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-02-09 18:51
+ *
+ * @Description: 98. 验证二叉搜索树
+ *
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ *
+ * 有效 二叉搜索树定义如下：
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ *  
+ *
+ * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+ */
+public class ValidBTree {
+}
