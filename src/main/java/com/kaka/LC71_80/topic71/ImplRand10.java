9d9b4e1c312dfec52c9755fdeaa5eda7c2834137
kakaacat <1912445179@qq.com>
2022-02-14 18:16:20 +0800

    LeetCode_Study(470. 用 Rand7() 实现 Rand10())

---
+++ src/main/java/com/kaka/LC71_80/topic71/ImplRand10.java
@@ -0,0 +1,20 @@
+package com.kaka.LC71_80.topic71;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-02-14 18:14
+ *
+ * @Description: 470. 用 Rand7() 实现 Rand10()
+ *
+ * 给定方法 rand7 可生成 [1,7] 范围内的均匀随机整数，试写一个方法 rand10 生成 [1,10] 范围内的均匀随机整数。
+ * 你只能调用 rand7() 且不能调用其他方法。请不要使用系统的 Math.random() 方法。
+ * 每个测试用例将有一个内部参数 n，即你实现的函数 rand10() 在测试时将被调用的次数。请注意，这不是传递给 rand10() 的参数。
+ *
+ * 链接：https://leetcode-cn.com/problems/implement-rand10-using-rand7
+ *
+ */
+public class ImplRand10 {
+    public int rand10(){
+
+    }
+}
