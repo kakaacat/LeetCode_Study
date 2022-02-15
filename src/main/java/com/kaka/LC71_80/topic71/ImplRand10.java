dc125d682745c717f9f00b2d85aee771ebd6952c
kakaacat <1912445179@qq.com>
2022-02-15 12:28:53 +0800

    LeetCode_Study(470. 用 Rand7() 实现 Rand10())

--- src/main/java/com/kaka/LC71_80/topic71/ImplRand10.java
+++ src/main/java/com/kaka/LC71_80/topic71/ImplRand10.java
@@ -53,7 +53,26 @@ public class ImplRand10 {
      *  *      则等概率映射范围调整为 [1...45]， 1 + num % 9
      *  *      if(num <= 45) return num % 9 + 1;
      *  */
-    public int rand10(){
 
+    private int rand7() {
+    }
+    public int rand10(){
+        while (true){
+            int num = (rand7() - 1) * 7 + rand7();
+            // 如果在40以内，那就直接返回
+            if (num <= 40) {
+                return 1 + num % 10;
+            }
+            //在41-49之间，利用随机数在操作一遍
+            num = (num - 40 - 1) * 7 + rand7();
+            if (num <= 60){
+                return 1 + num % 10;
+            }
+            //61-63,利用随机数再操作一遍
+            num = (num - 60 - 1) * 7 + rand7();
+            if (num <= 20) {
+                return 1 + num % 10;
+            }
+        }
     }
 }
