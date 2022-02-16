d9a274b526d3045e3379ee03f34f8e0b4084d78e
kakaacat <1912445179@qq.com>
2022-02-16 12:46:11 +0800

    LeetCode_Study(43. 字符串相乘)

---
+++ src/main/java/com/kaka/LC71_80/topic72/MultStrings.java
@@ -0,0 +1,18 @@
+package com.kaka.LC71_80.topic72;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-02-15 12:37
+ *
+ * @Description: 43. 字符串相乘
+ *
+ * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+ * 注意：不能使用任何内置的 BigInteger 库或直接将输入转换为整数。
+ *
+ * 链接：https://leetcode-cn.com/problems/multiply-strings
+ */
+public class MultStrings {
+    public String multiply(String num1, String num2) {
+
+    }
+}
