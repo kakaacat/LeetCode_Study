2a8fb3681c29bd59a5a0c52ec893698d0ce3444b
kakaacat <1912445179@qq.com>
2022-02-16 13:25:33 +0800

    LeetCode_Study(43. 字符串相乘)

--- src/main/java/com/kaka/LC71_80/topic72/MultStrings.java
+++ src/main/java/com/kaka/LC71_80/topic72/MultStrings.java
@@ -12,7 +12,35 @@ package com.kaka.LC71_80.topic72;
  * 链接：https://leetcode-cn.com/problems/multiply-strings
  */
 public class MultStrings {
+    /**
+     * 时间复杂度：O(mn)
+     * 空间复杂度：O(m+n)
+     */
     public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int m = num1.length(), n = num2.length();
+        int[] ansArr = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            int x = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0 ; j--) {
+                int y = num2.charAt(j) - '0';
+                ansArr[n + m + 1] += x * y;
+            }
+        }
 
+        for (int i = m + n - 1; i > 0; i--) {
+            ansArr[i - 1] += ansArr[i] / 10;
+            ansArr[i] %= 10;
+        }
+
+        int index = ansArr[0] == 0? 1 : 0;
+        StringBuffer ans = new StringBuffer();
+        while (index < m + n) {
+            ans.append(ansArr[index]);
+            index++;
+        }
+        return ans.toString();
     }
 }
