ed384c38591bea123269fd2b893859849174adc4
kakaacat <1912445179@qq.com>
2022-02-18 14:42:26 +0800

    LeetCode_Study(32. 最长有效括号)

--- src/main/java/com/kaka/LC71_80/topic74/LongestValidParentheses.java
+++ src/main/java/com/kaka/LC71_80/topic74/LongestValidParentheses.java
@@ -1,5 +1,8 @@
 package com.kaka.LC71_80.topic74;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @Author : kaka
  * @Date: 2022-02-18 13:05
@@ -44,5 +47,60 @@ public class LongestValidParentheses {
         }
 
         return max;
+    }
+
+    /**
+     * 栈
+     */
+    public int longestValidParentheses1(String s) {
+        int max = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '('){
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 正向逆向结合
+     */
+    public int longestValidParentheses2(String s){
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
     }
 }
