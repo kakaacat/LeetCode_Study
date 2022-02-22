07747b51287ceb945bc491f94ee8119a5703d395
kakaacat <1912445179@qq.com>
2022-02-22 11:42:01 +0800

    LeetCode_Study(322. 零钱兑换)

--- src/main/java/com/kaka/LC71_80/topic78/CoinChange.java
+++ src/main/java/com/kaka/LC71_80/topic78/CoinChange.java
@@ -1,5 +1,7 @@
 package com.kaka.LC71_80.topic78;
 
+import java.util.Arrays;
+
 /**
  * @Author : kaka
  * @Date: 2022-02-22 11:17
@@ -13,4 +15,20 @@ package com.kaka.LC71_80.topic78;
  * 链接：https://leetcode-cn.com/problems/coin-change
  */
 public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
 }
