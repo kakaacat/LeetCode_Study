887a4d34127ac26616f804372c8bad9b86cd4e9b
kakaacat <1912445179@qq.com>
2022-02-23 12:49:54 +0800

    LeetCode_Study(234. 回文链表)

---
+++ src/main/java/com/kaka/LC71_80/topic79/PalindromeLinkedList.java
@@ -0,0 +1,14 @@
+package com.kaka.LC71_80.topic79;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-02-23 12:49
+ *
+ * @Description: 234. 回文链表
+ *
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ *
+ * https://leetcode-cn.com/problems/palindrome-linked-list/
+ */
+public class PalindromeLinkedList {
+}
