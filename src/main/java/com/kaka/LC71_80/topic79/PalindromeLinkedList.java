83a345ffc7520d168ad9e177832adc1eed516d87
kakaacat <1912445179@qq.com>
2022-02-23 13:13:01 +0800

    LeetCode_Study(234. 回文链表)

--- src/main/java/com/kaka/LC71_80/topic79/PalindromeLinkedList.java
+++ src/main/java/com/kaka/LC71_80/topic79/PalindromeLinkedList.java
@@ -43,4 +43,57 @@ public class PalindromeLinkedList {
         }
         return true;
     }
+
+    /**
+     * 快慢指针
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     */
+    public boolean isPalindrome2(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        // 找到前半部分链表的尾节点并反转后半部分链表
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // 判断是否回文
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) {
+                result = false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        // 还原链表并返回结果
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    //反转链表
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr.next != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+    //快慢指针找中点
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
 }
