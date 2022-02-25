28bc3b3022465951f12c7410d15a7d7d92462ea3
kakaacat <1912445179@qq.com>
2022-02-25 10:37:35 +0800

    LeetCode_Study(48.旋转图像)

--- src/main/java/com/kaka/LC81_90/topic81/RotateImage.java
+++ src/main/java/com/kaka/LC81_90/topic81/RotateImage.java
@@ -12,5 +12,32 @@ package com.kaka.LC81_90.topic81;
  * 链接：https://leetcode-cn.com/problems/rotate-image
  */
 public class RotateImage {
+    /**
+     * 翻转代替旋转
+     *
+     * 时间复杂度：O(N^2)
+     * 空间复杂度：O(1)
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        //水平翻转
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - i][j];
+                matrix[n - 1 - i][j] = temp;
+            }
+        }
+
+        //主对角线翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
 
 }
