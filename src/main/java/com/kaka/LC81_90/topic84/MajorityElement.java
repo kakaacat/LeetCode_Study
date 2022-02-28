8383f16f2642eed1d362efaea34846ff6ece41b4
kakaacat <1912445179@qq.com>
2022-02-28 10:20:15 +0800

    LeetCode_Study(169. 多数元素)

--- src/main/java/com/kaka/LC81_90/topic84/MajorityElement.java
+++ src/main/java/com/kaka/LC81_90/topic84/MajorityElement.java
@@ -1,5 +1,7 @@
 package com.kaka.LC81_90.topic84;
 
+import java.util.Arrays;
+
 /**
  * @Author : kaka
  * @Date: 2022-02-28 10:11
@@ -13,4 +15,15 @@ package com.kaka.LC81_90.topic84;
  * 链接：https://leetcode-cn.com/problems/majority-element
  */
 public class MajorityElement {
+    /**
+     * 排序
+     *
+     * 时间复杂度：O(nlogn)。将数组排序的时间复杂度为O(nlogn)。
+     * 空间复杂度：O(logn)。如果使用语言自带的排序算法，
+     * 需要使用O(logn) 的栈空间。如果自己编写堆排序，则只需要使用 O(1) 的额外空间。
+     */
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
 }
