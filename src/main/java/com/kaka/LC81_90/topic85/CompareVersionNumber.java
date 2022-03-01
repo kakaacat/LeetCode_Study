92eafcc11b4c0d94b705cc1853e10602fef8a081
kakaacat <1912445179@qq.com>
2022-03-01 09:33:49 +0800

    LeetCode_Study(165. 比较版本号)

--- src/main/java/com/kaka/LC81_90/topic85/CompareVersionNumber.java
+++ src/main/java/com/kaka/LC81_90/topic85/CompareVersionNumber.java
@@ -24,6 +24,28 @@ package com.kaka.LC81_90.topic85;
  * 链接：https://leetcode-cn.com/problems/compare-version-numbers
  */
 public class CompareVersionNumber {
+    /**
+     * 字符串分割
+     */
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
 
-
+        for (int i = 0; i < v1.length || i < v2.length; i++) {
+            int x1 = 0, x2 = 0;
+            if (i < v1.length) {
+                x1 = Integer.parseInt(v1[i]);
+            }
+            if (i < v2.length) {
+                x2 = Integer.parseInt(v2[i]);
+            }
+            if (x1 < x2) {
+                return -1;
+            }
+            if (x1 > x2) {
+                return 1;
+            }
+        }
+        return 0;
+    }
 }
