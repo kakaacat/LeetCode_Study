a0ee810f2ce9131da3d9648f381c88002d7b08d7
kakaacat <1912445179@qq.com>
2022-03-02 09:17:35 +0800

    LeetCode_Study(226. 翻转二叉树)

---
+++ src/main/java/com/kaka/LC81_90/topic86/InvertBinaryTree.java
@@ -0,0 +1,19 @@
+package com.kaka.LC81_90.topic86;
+
+import com.kaka.LC21_30.topic27.TreeNode;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-03-02 09:16
+ *
+ * @Description: 226. 翻转二叉树
+ *
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ *
+ * https://leetcode-cn.com/problems/invert-binary-tree/
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+
+    }
+}
