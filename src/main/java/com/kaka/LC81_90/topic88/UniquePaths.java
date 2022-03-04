e4c4a71ab7d9ef5efac18c815610cc998db963e8
kakaacat <1912445179@qq.com>
2022-03-04 09:16:55 +0800

    LeetCode_Study(62. 不同路径)

---
+++ src/main/java/com/kaka/LC81_90/topic88/UniquePaths.java
@@ -0,0 +1,19 @@
+package com.kaka.LC81_90.topic88;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-03-04 09:15
+ *
+ * @Description: 62. 不同路径
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * 问总共有多少条不同的路径？
+ *
+ * 链接：https://leetcode-cn.com/problems/unique-paths
+ */
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+
+    }
+}
