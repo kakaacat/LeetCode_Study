b69e352540cf165adaad74591f7fcb04396afe96
kakaacat <1912445179@qq.com>
2022-03-04 09:30:07 +0800

    LeetCode_Study(62. 不同路径)

--- src/main/java/com/kaka/LC81_90/topic88/UniquePaths.java
+++ src/main/java/com/kaka/LC81_90/topic88/UniquePaths.java
@@ -13,7 +13,28 @@ package com.kaka.LC81_90.topic88;
  * 链接：https://leetcode-cn.com/problems/unique-paths
  */
 public class UniquePaths {
+    /**
+     * 动态规划
+     *
+     * 时间复杂度：O(mn)
+     * 空间复杂度：O(mn)
+     */
     public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
 
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
+
+
 }
