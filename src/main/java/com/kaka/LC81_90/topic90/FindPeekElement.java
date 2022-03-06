12892c8dac467aeb002ab5a2e6d834c7c964adfb
kakaacat <1912445179@qq.com>
2022-03-06 16:30:03 +0800

    LeetCode_Study(162. 寻找峰值)

---
+++ src/main/java/com/kaka/LC81_90/topic90/FindPeekElement.java
@@ -0,0 +1,17 @@
+package com.kaka.LC81_90.topic90;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-03-06 16:29
+ *
+ * @Description: 162. 寻找峰值
+ *
+ * 峰值元素是指其值严格大于左右相邻值的元素。
+ * 给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
+ * 你可以假设 nums[-1] = nums[n] = -∞ 。
+ * 你必须实现时间复杂度为 O(log n) 的算法来解决此问题。
+ *
+ * 链接：https://leetcode-cn.com/problems/find-peak-element
+ */
+public class FindPeekElement {
+}
