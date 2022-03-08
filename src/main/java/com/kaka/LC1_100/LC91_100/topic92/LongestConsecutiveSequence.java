eac0719d97063aa4d267334393db80fd58a9a741
kakaacat <1912445179@qq.com>
2022-03-08 09:25:43 +0800

    LeetCode_Study(128. 最长连续序列)

---
+++ src/main/java/com/kaka/LC1_100/LC91_100/topic92/LongestConsecutiveSequence.java
@@ -0,0 +1,21 @@
+package com.kaka.LC1_100.LC91_100.topic92;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-03-08 09:24
+ *
+ * @Description: 128. 最长连续序列
+ *
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ *
+ * 链接：https://leetcode-cn.com/problems/longest-consecutive-sequence
+ */
+public class LongestConsecutiveSequence {
+    /**
+     * hashMap
+     */
+    public int longestConsecutiveSequence(int[] nums) {
+
+    }
+}
