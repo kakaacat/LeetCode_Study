a9cce926b961236bec60756ba1e22f1f15533e89
kakaacat <1912445179@qq.com>
2022-03-10 10:11:22 +0800

    LeetCode_Study(468. 验证IP地址)

--- src/main/java/com/kaka/LC1_100/LC91_100/topic94/ValidIPAddress.java
+++ src/main/java/com/kaka/LC1_100/LC91_100/topic94/ValidIPAddress.java
@@ -2,6 +2,7 @@ package com.kaka.LC1_100.LC91_100.topic94;
 
 import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.util.regex.Pattern;
 
 /**
  * @Author : kaka
@@ -40,4 +41,17 @@ public class ValidIPAddress {
     /**
      * 正则表达式
      */
+    String chunkIPv4 = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+    Pattern patternIPv4 = Pattern.compile("^(" + chunkIPv4 + "\\.){3}" + chunkIPv4 + "$");
+    String chunkIPv6 = "([0-9a-fA-F]{1,4})";
+    Pattern patternIPv6 = Pattern.compile("^(" + chunkIPv6 + "\\:){7}" + chunkIPv6 + "$");
+
+    public String vaildIPAddress2(String IP) {
+        if (IP.contains(".")) {
+            return (patternIPv4.matcher(IP).matches()) ? "IPv4" : "Neither";
+        } else if (IP.contains(":")) {
+            return (patternIPv6.matcher(IP).matches()) ? "IPv6" : "Neither";
+        }
+        return "Neither";
+    }
 }
