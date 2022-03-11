8c5b12a6dfca90ce00793fd3a2d5f417c2231abf
kakaacat <1912445179@qq.com>
2022-03-11 14:52:19 +0800

    LeetCode_Study(24. 两两交换链表中的节点)

--- src/main/java/com/kaka/LC1_100/LC91_100/topic95/SwapTowNodes.java
+++ src/main/java/com/kaka/LC1_100/LC91_100/topic95/SwapTowNodes.java
@@ -1,5 +1,7 @@
 package com.kaka.LC1_100.LC91_100.topic95;
 
+import com.kaka.LC1_100.LC1_10.day02.ListNode;
+
 /**
  * @Author : kaka
  * @Date: 2022-03-11 14:44
@@ -12,5 +14,23 @@ package com.kaka.LC1_100.LC91_100.topic95;
  * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
  */
 public class SwapTowNodes {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = node1.next;
 
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
 }
