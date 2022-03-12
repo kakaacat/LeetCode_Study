ed215253402a7d8fa3abd78196fd298e256cea93
kakaacat <1912445179@qq.com>
2022-03-12 10:10:26 +0800

    LeetCode_Study(221. 最大正方形)

--- src/main/java/com/kaka/LC1_100/LC91_100/topic96/MaxSquare.java
+++ src/main/java/com/kaka/LC1_100/LC91_100/topic96/MaxSquare.java
@@ -58,4 +58,31 @@ public class MaxSquare {
         return maxSide * maxSide;
     }
 
+
+    /**
+     * 动态规划
+     */
+    public int maxSquare2(char[][] matrix) {
+        int maxSide = 0;
+        int row = matrix.length, col = matrix[0].length;
+        if (matrix == null || row == 0 || col == 0) {
+            return maxSide;
+        }
+
+        int[][] dp = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+
 }
