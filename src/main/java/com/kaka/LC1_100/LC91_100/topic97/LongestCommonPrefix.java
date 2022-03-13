009fa9ab206b210d42f69f5ef1aee091fd026284
kakaacat <1912445179@qq.com>
2022-03-13 19:10:34 +0800

    LeetCode_Study(14. 最长公共前缀)

---
+++ src/main/java/com/kaka/LC1_100/LC91_100/topic97/LongestCommonPrefix.java
@@ -0,0 +1,16 @@
+package com.kaka.LC1_100.LC91_100.topic97;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-03-13 19:09
+ *
+ * @Description: 14. 最长公共前缀
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * https://leetcode-cn.com/problems/longest-common-prefix/
+ */
+public class LongestCommonPrefix {
+
+}
