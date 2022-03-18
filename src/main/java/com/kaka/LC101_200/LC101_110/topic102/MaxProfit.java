018e09ebd4d8f9c478f084f37a49741f434d384e
kakaacat <1912445179@qq.com>
2022-03-18 10:49:20 +0800

    LeetCode_Study(122. 买卖股票的最佳时机 II)

--- src/main/java/com/kaka/LC101_200/LC101_110/topic102/MaxProfit.java
+++ src/main/java/com/kaka/LC101_200/LC101_110/topic102/MaxProfit.java
@@ -24,4 +24,42 @@ public class MaxProfit {
         }
         return ans;
     }
+
+    /**
+     * 动态规划
+     */
+    public int maxProfit2(int[] prices) {
+        int n = prices.length;
+        if (n < 2) {
+            return 0;
+        }
+
+        // cash：持有现金
+        // hold：持有股票
+        // 状态转移：cash → hold → cash → hold → cash → hold → cash
+        int cash = 0;
+        int hold = -prices[0];
+
+        // 0：持有现金
+        // 1：持有股票
+        // 状态转移：0 → 1 → 0 → 1 → 0 → 1 → 0
+        //int[][] dp = new int[len][2];
+        //dp[0][0] = 0;
+        //dp[0][1] = -prices[0];
+
+        int proCash = cash;
+        int proHold = hold;
+        for (int i = 1; i < n; i++) {
+            //dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            //dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+
+            cash = Math.max(proCash, proHold + prices[i]);
+            hold = Math.max(proHold, proCash - prices[i]);
+
+            proCash = cash;
+            proHold = hold;
+        }
+        //return dp[len - 1][0];
+        return cash;
+    }
 }
