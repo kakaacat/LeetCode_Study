f2266598f7c607d787293ebb497c4ba554b2f419
kakaacat <1912445179@qq.com>
2022-03-21 10:19:29 +0800

    LeetCode_Study(198. 打家劫舍)

--- src/main/java/com/kaka/LC101_200/LC101_110/topic104/HouseRobber.java
+++ src/main/java/com/kaka/LC101_200/LC101_110/topic104/HouseRobber.java
@@ -13,5 +13,23 @@ package com.kaka.LC101_200.LC101_110.topic104;
  * 链接：https://leetcode-cn.com/problems/house-robber
  */
 public class HouseRobber {
-
+    /**
+     * 动态规划
+     */
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 0 || nums == null) {
+            return 0;
+        }
+        if (len == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < len; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
 }
