2df1403f7a89e4a1ff74460319b1b366cd391193
kakaacat <1912445179@qq.com>
2022-03-22 10:11:02 +0800

    LeetCode_Study(152. 乘积最大子数组)

---
+++ src/main/java/com/kaka/LC101_200/LC101_110/topic105/MaxProduct.java
@@ -0,0 +1,18 @@
+package com.kaka.LC101_200.LC101_110.topic105;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-03-22 09:35
+ *
+ * @Description: 152. 乘积最大子数组
+ *
+ * 给你一个整数数组 nums ，请你找出数组中乘积最大的非空连续子数组（该子数组中至少包含一个数字），
+ * 并返回该子数组所对应的乘积。
+ * 测试用例的答案是一个 32-位 整数。
+ * 子数组 是数组的连续子序列。
+ *
+ * 链接：https://leetcode-cn.com/problems/maximum-product-subarray
+ */
+public class MaxProduct {
+
+}
