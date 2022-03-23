4d2d26c4c3d81644792a701f8ba4ecffbc0ce8da
kakaacat <1912445179@qq.com>
2022-03-23 09:54:28 +0800

    LeetCode_Study(剑指 Offer 36. 二叉搜索树与双向链表)

--- src/main/java/com/kaka/LC101_200/LC101_110/topic106/TreeToDoublyList.java
+++ src/main/java/com/kaka/LC101_200/LC101_110/topic106/TreeToDoublyList.java
@@ -1,5 +1,6 @@
 package com.kaka.LC101_200.LC101_110.topic106;
 
+
 /**
  * @Author : kaka
  * @Date: 2022-03-23 09:44
@@ -12,4 +13,33 @@ package com.kaka.LC101_200.LC101_110.topic106;
  * https://leetcode-cn.com/problems/er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/
  */
 public class TreeToDoublyList {
+    /**
+     * 中序
+     */
+    Node head, prev;
+    public Node treeToDoublyList(Node root) {
+        if (root == null) {
+            return null;
+        }
+        dfs(root);
+        prev.right = head;
+        head.left = prev;
+        return head;
+    }
+
+    private void dfs(Node root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (prev == null) {
+            prev = root;
+            head = root;
+        } else {
+            prev.right = root;
+            root.left = prev;
+            prev = root;
+        }
+        dfs(root.right);
+    }
 }
