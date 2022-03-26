40be44b4d7c48ffc4bd49a12a7629f6f83b8de6d
kakaacat <1912445179@qq.com>
2022-03-26 10:28:35 +0800

    LeetCode_Study(剑指 Offer 09. 用两个栈实现队列)

--- src/main/java/com/kaka/LC101_200/LC101_110/topic109/CQueue.java
+++ src/main/java/com/kaka/LC101_200/LC101_110/topic109/CQueue.java
@@ -15,5 +15,28 @@ import java.util.Deque;
  * 链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
  */
 public class CQueue {
+    private Deque<Integer> inStack;
+    private Deque<Integer> outStack;
 
+    public CQueue(){
+        inStack = new ArrayDeque<>();
+        outStack = new ArrayDeque<>();
+    }
+
+    public void appendTail(int value) {
+        inStack.push(value);
+    }
+
+    public int deleteHead() {
+        if (outStack.isEmpty()) {
+            if (inStack.isEmpty()) {
+                return -1;
+            } else {
+                while (!inStack.isEmpty()) {
+                    outStack.push(inStack.pop());
+                }
+            }
+        }
+        return outStack.pop();
+    }
 }
