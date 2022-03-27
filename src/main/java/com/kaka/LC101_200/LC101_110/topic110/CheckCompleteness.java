64f9eaf3f26cf7a591f7960fea5c3cdda5a3d6a7
kakaacat <1912445179@qq.com>
2022-03-27 11:35:08 +0800

    LeetCode_Study(958. 二叉树的完全性检验)

---
+++ src/main/java/com/kaka/LC101_200/LC101_110/topic110/CheckCompleteness.java
@@ -0,0 +1,16 @@
+package com.kaka.LC101_200.LC101_110.topic110;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-03-27 11:34
+ *
+ * @Description: 958. 二叉树的完全性检验
+ *
+ * 给定一个二叉树的 root ，确定它是否是一个 完全二叉树 。
+ * 在一个 完全二叉树 中，除了最后一个关卡外，所有关卡都是完全被填满的，
+ * 并且最后一个关卡中的所有节点都是尽可能靠左的。它可以包含 1 到 2h 节点之间的最后一级 h 。
+ *
+ * 链接：https://leetcode-cn.com/problems/check-completeness-of-a-binary-tree
+ */
+public class CheckCompleteness {
+}
