376ce77da59931098a4119440d1c6ae4e69e68ad
kakaacat <1912445179@qq.com>
2022-03-28 09:57:19 +0800

    LeetCode_Study(283. 移动零)

--- src/main/java/com/kaka/LC101_200/LC111_120/topic111/MoveZeroes.java
+++ src/main/java/com/kaka/LC101_200/LC111_120/topic111/MoveZeroes.java
@@ -12,5 +12,27 @@ package com.kaka.LC101_200.LC111_120.topic111;
  * https://leetcode-cn.com/problems/move-zeroes/
  */
 public class MoveZeroes {
+    /**
+     * 交换，但不保证非零元素的相对顺序
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        int r = 0, l = nums.length - 1;
+        while (r < l) {
+            if (nums[r] == 0) {
+                while (nums[l] == 0) {
+                    l--;
+                }
+                swap(nums, r, l);
+                l--;
+            }
+            r++;
+        }
+    }
 
+    private void swap(int[] nums, int r, int l) {
+        int temp = nums[r];
+        nums[r] = nums[l];
+        nums[l] = temp;
+    }
 }
