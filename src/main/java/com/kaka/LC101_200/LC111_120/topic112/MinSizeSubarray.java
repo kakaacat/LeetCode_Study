fce6e178773a2c0a73caef4d770443a802ec520c
kakaacat <1912445179@qq.com>
2022-03-29 10:03:18 +0800

    LeetCode_Study(209. 长度最小的子数组)

--- src/main/java/com/kaka/LC101_200/LC111_120/topic112/MinSizeSubarray.java
+++ src/main/java/com/kaka/LC101_200/LC111_120/topic112/MinSizeSubarray.java
@@ -13,4 +13,26 @@ package com.kaka.LC101_200.LC111_120.topic112;
  * 链接：https://leetcode-cn.com/problems/minimum-size-subarray-sum
  */
 public class MinSizeSubarray {
+    /**
+     * 滑动窗口
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        }
+        int start = 0, end = 0, sum = 0;
+        int ans = Integer.MAX_VALUE;
+        while (end < len) {
+            sum += nums[end];
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
 }
