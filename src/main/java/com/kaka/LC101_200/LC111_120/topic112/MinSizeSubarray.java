b12597852877b27a28f9cd40cd03cb704b427486
kakaacat <1912445179@qq.com>
2022-03-29 09:53:58 +0800

    LeetCode_Study(209. 长度最小的子数组)

---
+++ src/main/java/com/kaka/LC101_200/LC111_120/topic112/MinSizeSubarray.java
@@ -0,0 +1,16 @@
+package com.kaka.LC101_200.LC111_120.topic112;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-03-29 09:53
+ *
+ * @Description: 209. 长度最小的子数组
+ *
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。
+ * 如果不存在符合条件的子数组，返回 0 。
+ *
+ * 链接：https://leetcode-cn.com/problems/minimum-size-subarray-sum
+ */
+public class MinSizeSubarray {
+}
