05056ce3336c6111d642e0c49c177d2c5649834f
kakaacat <1912445179@qq.com>
2022-04-02 10:28:12 +0800

    LeetCode_Study(归并排序)

--- src/main/java/com/kaka/LC101_200/LC111_120/topic115/SortArray.java
+++ src/main/java/com/kaka/LC101_200/LC111_120/topic115/SortArray.java
@@ -12,7 +12,7 @@ package com.kaka.LC101_200.LC111_120.topic115;
  */
 public class SortArray {
     /**
-     * 归并排序
+     * 归并排序--分治法
      */
     int[] temp;
     public int[] sortArray(int[] nums) {
@@ -25,11 +25,14 @@ public class SortArray {
         if (l >= r) {
             return;
         }
+        //分
         int mid = (l + r) / 2;
         mergeSort(nums, l, mid);
         mergeSort(nums, mid + 1, r);
+        //治
         int i = l, j = mid + 1;
         int cur = 0;
+        // 把较小的数先移到新数组中
         while (i <= mid && j <= r) {
             if (nums[i] < nums[j]) {
                 temp[cur++] = nums[i++];
@@ -37,15 +40,18 @@ public class SortArray {
                 temp[cur++] = nums[j++];
             }
         }
+        // 把左边剩余的数移入数组
         while (i <= mid) {
             temp[cur++] = nums[i++];
         }
+        // 把右边边剩余的数移入数组
         while (j <= r) {
             temp[cur++] = nums[j++];
         }
 
+        // 把新数组中的数覆盖nums数组
         for (int k = 0; k < r - l + 1; k++) {
-            nums[k + 1] = temp[k];
+            nums[k + l] = temp[k];
         }
     }
 }
