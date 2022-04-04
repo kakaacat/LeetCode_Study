49aaeb1f67f770e047f0a6824b7940c20ad5b112
kakaacat <1912445179@qq.com>
2022-04-04 19:48:23 +0800

    LeetCode_Study(79. 单词搜索)

--- src/main/java/com/kaka/LC101_200/LC111_120/topic118/WordSearch.java
+++ src/main/java/com/kaka/LC101_200/LC111_120/topic118/WordSearch.java
@@ -14,5 +14,61 @@ package com.kaka.LC101_200.LC111_120.topic118;
  * 链接：https://leetcode-cn.com/problems/word-search
  */
 public class WordSearch {
+    /**
+     * dfs + 回溯
+     */
+    private static final int[][] DIRECTIONS = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    private int rows;
+    private int cols;
+    private int len;
+    private boolean[][] visited;
+    private char[] charArray;
+    private char[][] board;
+
+    public boolean exist(char[][] board, String word) {
+        rows = board.length;
+        if (rows == 0) {
+            return false;
+        }
+        cols = board[0].length;
+        visited = new boolean[rows][cols];
+
+        this.len = word.length();
+        this.charArray = word.toCharArray();
+        this.board = board;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int i, int j, int begin) {
+        if (begin == len - 1) {
+            return board[i][j] == charArray[begin];
+        }
+
+        if (board[i][j] == charArray[begin]) {
+            visited[i][j] = true;
+            for (int[] direction : DIRECTIONS) {
+                int x = i + direction[0];
+                int y = j + direction[1];
+                if (inArea(x, y) && !visited[x][y]) {
+                    if (dfs(x, y, begin + 1)) {
+                        return true;
+                    }
+                }
+            }
+            visited[i][j] = false;
+        }
+        return false;
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < rows && y >= 0 && y < cols;
+    }
 
 }
