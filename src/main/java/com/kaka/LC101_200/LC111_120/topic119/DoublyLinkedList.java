be235be0825e93baf649347e1bfc71832e0c93ce
kakaacat <1912445179@qq.com>
2022-04-05 19:55:40 +0800

    LeetCode_Study(460. LFU 缓存)

---
+++ src/main/java/com/kaka/LC101_200/LC111_120/topic119/DoublyLinkedList.java
@@ -0,0 +1,42 @@
+package com.kaka.LC101_200.LC111_120.topic119;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-04-05 19:47
+ */
+public class DoublyLinkedList {
+    Node dummyHead, dummyTail;
+    public int size;
+
+    public DoublyLinkedList() {
+        dummyHead = new Node();
+        dummyTail = new Node();
+        dummyHead.next = dummyTail;
+        dummyTail.prev = dummyHead;
+        size = 0;
+    }
+
+    public void addFirst(Node node) {
+        Node prevHead = dummyHead.next;
+        node.prev = dummyHead;
+        dummyHead.next = node;
+        node.next = prevHead;
+        prevHead.prev = node;
+        size++;
+    }
+
+    public void remove(Node node) {
+        Node prev = node.prev, next = node.next;
+        prev.next = next;
+        next.prev = prev;
+        size--;
+    }
+
+    public Node getHead() {
+        return dummyHead.next;
+    }
+
+    public Node getTail() {
+        return dummyTail.prev;
+    }
+}
---
+++ src/main/java/com/kaka/LC101_200/LC111_120/topic119/Node.java
@@ -0,0 +1,20 @@
+package com.kaka.LC101_200.LC111_120.topic119;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-04-05 19:45
+ */
+public class Node {
+    public int key, val, freq;
+    Node prev, next;
+
+    public Node() {
+        this(-1, -1, 0);
+    }
+
+    public Node(int key, int val, int freq) {
+        this.key = key;
+        this.val = val;
+        this.freq = freq;
+    }
+}
