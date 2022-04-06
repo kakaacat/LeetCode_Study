723a719709a9df0fa36c748f610a26b584b6c839
kakaacat <1912445179@qq.com>
2022-04-06 09:56:02 +0800

    LeetCode_Study(排序奇升偶降链表)

---
+++ src/main/java/com/kaka/LC101_200/LC111_120/topic120/SortLinkedList.java
@@ -0,0 +1,17 @@
+package com.kaka.LC101_200.LC111_120.topic120;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-04-06 09:55
+ *
+ * @Description: 排序奇升偶降链表
+ *
+ * 链接：https://www.nowcoder.com/questionTerminal/3a188e9c06ce4844b031713b82784a2a?source=relative
+ * 来源：牛客网
+ *
+ * 给定一个奇数位升序，偶数位降序的链表，返回对其排序后的链表。
+ * 题面解释：例如链表 1->3->2->2->3->1 是奇数位升序偶数位降序的链表，而 1->3->2->2->3->2 则不符合题目要求。
+ */
+public class SortLinkedList {
+
+}
