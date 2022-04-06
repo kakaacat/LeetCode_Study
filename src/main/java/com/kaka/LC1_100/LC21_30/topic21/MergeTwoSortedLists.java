b1b7864b74c1b44522424bfc4a57c16a33fe1352
kakaacat <1912445179@qq.com>
2022-04-06 10:23:01 +0800

    LeetCode_Study(优化代码结构)

--- src/main/java/com/kaka/LC1_100/LC21_30/topic21/MergeTwoSortedLists.java
+++ src/main/java/com/kaka/LC1_100/LC21_30/topic21/MergeTwoSortedLists.java
@@ -48,21 +48,21 @@ public class MergeTwoSortedLists {
      * 空间复杂度：O(1)
      */
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode preHead = new ListNode(0);
-        ListNode prev = preHead;
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
         while(list1 != null && list2 != null){
             if(list1.val <= list2.val){
-                prev.next = list1;
+                curr.next = list1;
                 list1 = list1.next;
             }else{
-                prev.next = list2;
+                curr.next = list2;
                 list2 = list2.next;
             }
-            prev = prev.next;
+            curr = curr.next;
         }
 
-        prev.next = list1 == null ? list2 : list1;
+        curr.next = list1 == null ? list2 : list1;
 
-        return preHead.next;
+        return dummyHead.next;
     }
 }
