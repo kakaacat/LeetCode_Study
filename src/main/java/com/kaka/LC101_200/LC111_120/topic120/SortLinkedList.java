da011503b19d9f26ac585e77da23466946c40d44
kakaacat <1912445179@qq.com>
2022-04-06 10:21:42 +0800

    LeetCode_Study(排序奇升偶降链表)

--- src/main/java/com/kaka/LC101_200/LC111_120/topic120/SortLinkedList.java
+++ src/main/java/com/kaka/LC101_200/LC111_120/topic120/SortLinkedList.java
@@ -1,17 +1,73 @@
 package com.kaka.LC101_200.LC111_120.topic120;
 
+import com.kaka.LC1_100.LC1_10.day02.ListNode;
+
 /**
  * @Author : kaka
  * @Date: 2022-04-06 09:55
  *
  * @Description: 排序奇升偶降链表
  *
- * 链接：https://www.nowcoder.com/questionTerminal/3a188e9c06ce4844b031713b82784a2a?source=relative
- * 来源：牛客网
+ * 给定一个奇数位升序，偶数位降序的链表，将其重新排序。
  *
- * 给定一个奇数位升序，偶数位降序的链表，返回对其排序后的链表。
- * 题面解释：例如链表 1->3->2->2->3->1 是奇数位升序偶数位降序的链表，而 1->3->2->2->3->2 则不符合题目要求。
+ * 输入: 1->8->3->6->5->4->7->2->NULL
+ * 输出: 1->2->3->4->5->6->7->8->NULL
  */
 public class SortLinkedList {
+    public ListNode sortLinkedList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        //先把奇数位链表和偶数位链表拆开
+        ListNode oddCur = head;
+        ListNode evenCur = head.next;
+        ListNode oddHead = head;
+        ListNode evenHead = head.next;
+        while (evenCur != null) {
+            oddCur.next = evenCur.next;
+            if (oddCur.next != null) {
+                evenCur.next = oddCur.next.next;
+            }
+            oddCur = oddCur.next;
+            evenCur = evenCur.next;
+        }
+        //然后把偶数位链表逆序
+        evenHead = reverseList(evenHead);
+        //最后合并两个链表
+        return mergeList(oddHead, evenHead);
+    }
 
+    private ListNode mergeList(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode();
+        ListNode curr = dummyHead;
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                curr.next = head1;
+                head1 = head1.next;
+            } else {
+                curr.next = head2;
+                head2 = head2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = head1 != null ? head1 : head2;
+
+        return dummyHead.next;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode n = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = n;
+        }
+        return prev;
+    }
 }
