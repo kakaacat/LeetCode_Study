e52e435782a7b7f30eb428b29cf5f93f21197b80
kakaacat <1912445179@qq.com>
2022-04-07 10:12:55 +0800

    LeetCode_Study(518. 零钱兑换 II)

--- src/main/java/com/kaka/LC101_200/LC121_130/topic121/CoinChangeII.java
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic121/CoinChangeII.java
@@ -14,4 +14,18 @@ package com.kaka.LC101_200.LC121_130.topic121;
  * 链接：https://leetcode-cn.com/problems/coin-change-2
  */
 public class CoinChangeII {
+    /**
+     * 动态规划
+     */
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[amount];
+    }
 }
