6d62803ed88297f75bddc1fe830c4eb789bc4a63
kakaacat <1912445179@qq.com>
2022-04-08 17:30:43 +0800

    LeetCode_Study(560. 和为 K 的子数组)

---
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic122/SubarraySumEqualsK.java
@@ -0,0 +1,15 @@
+package com.kaka.LC101_200.LC121_130.topic122;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-04-08 17:30
+ *
+ * @Description: 560. 和为 K 的子数组
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+ *
+ * https://leetcode-cn.com/problems/subarray-sum-equals-k/
+ */
+public class SubarraySumEqualsK {
+
+}
