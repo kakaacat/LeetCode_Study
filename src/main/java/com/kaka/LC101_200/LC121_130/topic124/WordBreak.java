ac2a1d4978db0063d9c208f2b8a0d297889ddaf9
kakaacat <1912445179@qq.com>
2022-04-11 10:28:38 +0800

    LeetCode_Study(139. 单词拆分)

---
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic124/WordBreak.java
@@ -0,0 +1,15 @@
+package com.kaka.LC101_200.LC121_130.topic124;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-04-11 10:28
+ *
+ * @Description: 139. 单词拆分
+ *
+ * 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。请你判断是否可以利用字典中出现的单词拼接出 s 。
+ * 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+ *
+ * 链接：https://leetcode-cn.com/problems/word-break
+ */
+public class WordBreak {
+}
