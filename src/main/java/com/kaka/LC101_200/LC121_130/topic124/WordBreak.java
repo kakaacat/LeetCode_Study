7845292d1e11a6cf32cbdefa2a618e9c2add1389
kakaacat <1912445179@qq.com>
2022-04-11 10:43:07 +0800

    LeetCode_Study(139. 单词拆分)

--- src/main/java/com/kaka/LC101_200/LC121_130/topic124/WordBreak.java
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic124/WordBreak.java
@@ -1,5 +1,9 @@
 package com.kaka.LC101_200.LC121_130.topic124;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @Author : kaka
  * @Date: 2022-04-11 10:28
@@ -12,4 +16,25 @@ package com.kaka.LC101_200.LC121_130.topic124;
  * 链接：https://leetcode-cn.com/problems/word-break
  */
 public class WordBreak {
+    /**
+     * 动态规划
+     * @param s
+     * @param wordDict
+     * @return
+     */
+    public boolean workBreak(String s, List<String> wordDict) {
+        int len = s.length();
+        Set<String> set = new HashSet(wordDict);
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[len];
+    }
 }
