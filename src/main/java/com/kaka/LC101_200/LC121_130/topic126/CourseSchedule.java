ced5c910d93ef3ba2bfec69982506cd394e70c9f
kakaacat <1912445179@qq.com>
2022-04-13 20:13:55 +0800

    LeetCode_Study(207. 课程表)

---
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic126/CourseSchedule.java
@@ -0,0 +1,18 @@
+package com.kaka.LC101_200.LC121_130.topic126;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-04-13 20:13
+ *
+ * @Description: 207. 课程表
+ *
+ * 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+ * 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，
+ * 其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+ * 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+ * 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+ *
+ * 链接：https://leetcode-cn.com/problems/course-schedule
+ */
+public class CourseSchedule {
+}
