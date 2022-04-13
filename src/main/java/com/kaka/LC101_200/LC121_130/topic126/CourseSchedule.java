43760735b8e99862b68f424e543549605059ae96
kakaacat <1912445179@qq.com>
2022-04-13 20:26:58 +0800

    LeetCode_Study(207. 课程表)

--- src/main/java/com/kaka/LC101_200/LC121_130/topic126/CourseSchedule.java
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic126/CourseSchedule.java
@@ -1,5 +1,8 @@
 package com.kaka.LC101_200.LC121_130.topic126;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author : kaka
  * @Date: 2022-04-13 20:13
@@ -15,4 +18,49 @@ package com.kaka.LC101_200.LC121_130.topic126;
  * 链接：https://leetcode-cn.com/problems/course-schedule
  */
 public class CourseSchedule {
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+
+    /**
+     * dfs
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<Integer>());
+        }
+
+        visited = new int[numCourses];
+
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+
+        for (int i = 0; i < numCourses && valid; i++) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+
+    private void dfs(int n) {
+        visited[n] = 1;
+        for (int v : edges.get(n)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[n] = 2;
+    }
 }
