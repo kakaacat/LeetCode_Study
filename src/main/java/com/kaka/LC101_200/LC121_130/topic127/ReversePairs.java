0037d59ed4b5e495f3a6b03b5823a79d8dda0322
kakaacat <1912445179@qq.com>
2022-04-14 20:38:18 +0800

    LeetCode_Study(剑指 Offer 51. 数组中的逆序对)

---
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic127/ReversePairs.java
@@ -0,0 +1,14 @@
+package com.kaka.LC101_200.LC121_130.topic127;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-04-14 20:37
+ *
+ * @Description: 剑指 Offer 51. 数组中的逆序对
+ *
+ * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+ *
+ * https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof/
+ */
+public class ReversePairs {
+}
