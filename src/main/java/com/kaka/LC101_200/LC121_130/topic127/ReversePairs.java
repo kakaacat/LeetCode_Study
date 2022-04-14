0ebee198c2f56d34f0c638c88215b010ef560255
kakaacat <1912445179@qq.com>
2022-04-14 20:54:28 +0800

    LeetCode_Study(剑指 Offer 51. 数组中的逆序对)

--- src/main/java/com/kaka/LC101_200/LC121_130/topic127/ReversePairs.java
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic127/ReversePairs.java
@@ -1,5 +1,7 @@
 package com.kaka.LC101_200.LC121_130.topic127;
 
+import com.sun.org.apache.bcel.internal.generic.RET;
+
 /**
  * @Author : kaka
  * @Date: 2022-04-14 20:37
@@ -11,4 +13,71 @@ package com.kaka.LC101_200.LC121_130.topic127;
  * https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof/
  */
 public class ReversePairs {
+    /**
+     * 归并
+     */
+    public int reversePairs(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        //不改变原数组
+        int[] copy = new int[len];
+        for (int i = 0; i < len; i++) {
+            copy[i] = nums[i];
+        }
+
+        int[] temp = new int[len];
+        return reversePairs(copy, 0, len - 1, temp);
+    }
+
+    private int reversePairs(int[] nums, int left, int right, int[] temp) {
+        if (left == right) {
+            return 0;
+        }
+
+        int mid = left + (right - left) / 2;
+        int leftPairs = reversePairs(nums, left, mid, temp);
+        int rightPairs = reversePairs(nums, mid + 1, right, temp);
+
+        if (nums[mid] <= nums[mid + 1]) {
+            return leftPairs + rightPairs;
+        }
+
+        int crossPairs = mergerAndCount(nums, left, mid, right, temp);\
+        return leftPairs + rightPairs + crossPairs;
+    }
+
+    private int mergerAndCount(int[] nums, int left, int mid, int right, int[] temp) {
+        int i = left;
+        int j = mid + 1;
+        int t = 0;
+        //计数器
+        int count = 0;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp[t++] = nums[i++];
+            } else {
+                temp[t++] = nums[j++];
+                count += (mid - i) + 1;
+            }
+        }
+
+        while (i <= mid) {
+            temp[t++] = nums[i++];
+        }
+        while (j <= right) {
+            temp[t++] = nums[j++];
+        }
+
+        //将temp中的元素  全部都copy到原数组里边去
+        t = 0;
+        while (left <= right) {
+            nums[left++] = temp[t++];
+        }
+
+        return count;
+    }
 }
