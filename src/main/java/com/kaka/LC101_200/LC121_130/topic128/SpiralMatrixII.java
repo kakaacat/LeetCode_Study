4eff8b5bd230ec7ce0c5e551180390fe5e2c0691
kakaacat <1912445179@qq.com>
2022-04-15 17:19:21 +0800

    LeetCode_Study(59. 螺旋矩阵 II)

--- src/main/java/com/kaka/LC101_200/LC121_130/topic128/SpiralMatrixII.java
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic128/SpiralMatrixII.java
@@ -11,4 +11,26 @@ package com.kaka.LC101_200.LC121_130.topic128;
  * https://leetcode-cn.com/problems/spiral-matrix-ii/
  */
 public class SpiralMatrixII {
+    //模拟
+    public int[][] generateMatrix(int n) {
+        int maxNum = n * n;
+        int curNum = 1;
+        int[][] matrix = new int[n][n];
+        int row = 0, col = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1 ,0}};    //右下左上
+        int directionIndex = 0;
+        while (curNum <= maxNum) {
+            matrix[row][col] = curNum;
+            curNum++;
+            int newRow = row + directions[directionIndex][0];
+            int newCol = col + directions[directionIndex][1];
+            if (newRow < 0 || newRow >= n || newCol < 0 || newCol >= n || matrix[newRow][newCol] != 0) {
+                directionIndex = (directionIndex + 1) % 4;
+            }
+            row += directions[directionIndex][0];
+            col += directions[directionIndex][1];
+        }
+
+        return matrix;
+    }
 }
