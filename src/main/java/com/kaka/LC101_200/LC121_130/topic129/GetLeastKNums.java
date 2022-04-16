9a6a0f4a509ceed3f54784e81a0cf01a2b1258c7
kakaacat <1912445179@qq.com>
2022-04-16 13:23:04 +0800

    LeetCode_Study(剑指 Offer 40. 最小的k个数)

--- src/main/java/com/kaka/LC101_200/LC121_130/topic129/GetLeastKNums.java
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic129/GetLeastKNums.java
@@ -1,6 +1,8 @@
 package com.kaka.LC101_200.LC121_130.topic129;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * @Author : kaka
@@ -25,5 +27,33 @@ public class GetLeastKNums {
         return ret;
     }
 
-
+    /**
+     * 堆
+     */
+    public int[] getLeastNumber2(int[] arr, int k) {
+        int[] vec = new int[k];
+        // 排除 0 的情况
+        if (k == 0) {
+            return vec;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer num1, Integer num2) {
+                return num2 - num1;
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            queue.offer(arr[i]);
+        }
+        for (int i = k; i < arr.length; ++i) {
+            if (queue.peek() > arr[i]) {
+                queue.poll();
+                queue.offer(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            vec[i] = queue.poll();
+        }
+        return vec;
+    }
 }
