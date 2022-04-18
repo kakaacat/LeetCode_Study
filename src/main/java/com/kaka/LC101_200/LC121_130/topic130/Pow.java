10d16d049b32d673d0e8804f983549dba7d37667
kakaacat <1912445179@qq.com>
2022-04-18 11:06:19 +0800

    LeetCode_Study(50. Pow(x, n))

---
+++ src/main/java/com/kaka/LC101_200/LC121_130/topic130/Pow.java
@@ -0,0 +1,14 @@
+package com.kaka.LC101_200.LC121_130.topic130;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-04-18 11:05
+ *
+ * @Description: 50. Pow(x, n)
+ *
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn ）。
+ *
+ * https://leetcode-cn.com/problems/powx-n/
+ */
+public class Pow {
+}
