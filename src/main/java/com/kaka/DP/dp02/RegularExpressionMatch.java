d1337f57f148d91394c859cf5e587cd25ef93d35
kakaacat <1912445179@qq.com>
2022-04-23 20:30:01 +0800

    动态规划专练--（2)正则表达式匹配

--- src/main/java/com/kaka/DP/dp02/RegularExpressionMatch.java
+++ src/main/java/com/kaka/DP/dp02/RegularExpressionMatch.java
@@ -14,5 +14,38 @@ package com.kaka.DP.dp02;
  * 链接：https://leetcode-cn.com/problems/regular-expression-matching
  */
 public class RegularExpressionMatch {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
 
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (match(s, p, i, j)) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                } else {
+                    if (match(s, p, i, j)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    private boolean match(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
 }
