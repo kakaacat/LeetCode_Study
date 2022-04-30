87549a6003430f00fded98d2e84c92406099bd5f
kakaacat <1912445179@qq.com>
2022-04-30 09:57:20 +0800

    动态规划专练--（3)括号生成

---
+++ src/main/java/com/kaka/DP/dp03/GenerateParenthes.java
@@ -0,0 +1,14 @@
+package com.kaka.DP.dp03;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-04-30 09:56
+ *
+ * @Description: 22. 括号生成
+ *
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ *
+ * https://leetcode-cn.com/problems/generate-parentheses/
+ */
+public class GenerateParenthes {
+}
