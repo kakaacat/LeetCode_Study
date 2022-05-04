16ef6d7433f9c571d59d3b5d02a4065bedff8969
kakaacat <1912445179@qq.com>
2022-05-04 17:15:38 +0800

    动态规划专练--（3)括号生成

--- src/main/java/com/kaka/DP/dp03/GenerateParenthes.java
+++ src/main/java/com/kaka/DP/dp03/GenerateParenthes.java
@@ -14,22 +14,33 @@ import java.util.List;
  * https://leetcode-cn.com/problems/generate-parentheses/
  */
 public class GenerateParenthes {
+    /**
+     * 动态规划：
+     * dp[i]表示i组括号的所有有效组合
+     * dp[i] = "(" + dp[p]的所有有效组合+ ")" + dp[q]的组合，其中 1 + p + q = i , p从0遍历到i-1, q则相应从i-1到0
+     */
     public List<String> generateParenthes(int n) {
         List<LinkedList<String>> result = new LinkedList<LinkedList<String>>();
         if (n == 0) {
             return result.get(0);
         }
+        //0组括号时记为None
         LinkedList<String> list0 = new LinkedList<>();
         list0.add("");
         result.add(list0);
+        //1组括号只有一种情况
         LinkedList<String> list1 = new LinkedList<>();
         list1.add("()");
         result.add(list1);
 
+        //开始计算i组括号时的括号组合
         for (int i = 2; i <= n; i++) {
             LinkedList<String> temp = new LinkedList<>();
+            //开始遍历 p q ，其中p+q=i-1 , j 作为索引
             for (int j = 0; j < i; j++) {
+                //p = j 时的括号组合情况
                 List<String> str1 = result.get(j);
+                //q = (i-1) - j 时的括号组合情况
                 List<String> str2 = result.get(i - 1 - j);
                 for (String s1 : str1) {
                     for (String s2 : str2) {
