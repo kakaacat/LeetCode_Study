e8f1b05e3662dd0c90c1466e3ee0562873ab415b
kakaacat <1912445179@qq.com>
2022-05-04 17:12:48 +0800

    动态规划专练--（3)括号生成

--- src/main/java/com/kaka/DP/dp03/GenerateParenthes.java
+++ src/main/java/com/kaka/DP/dp03/GenerateParenthes.java
@@ -1,5 +1,8 @@
 package com.kaka.DP.dp03;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @Author : kaka
  * @Date: 2022-04-30 09:56
@@ -11,4 +14,32 @@ package com.kaka.DP.dp03;
  * https://leetcode-cn.com/problems/generate-parentheses/
  */
 public class GenerateParenthes {
+    public List<String> generateParenthes(int n) {
+        List<LinkedList<String>> result = new LinkedList<LinkedList<String>>();
+        if (n == 0) {
+            return result.get(0);
+        }
+        LinkedList<String> list0 = new LinkedList<>();
+        list0.add("");
+        result.add(list0);
+        LinkedList<String> list1 = new LinkedList<>();
+        list1.add("()");
+        result.add(list1);
+
+        for (int i = 2; i <= n; i++) {
+            LinkedList<String> temp = new LinkedList<>();
+            for (int j = 0; j < i; j++) {
+                List<String> str1 = result.get(j);
+                List<String> str2 = result.get(i - 1 - j);
+                for (String s1 : str1) {
+                    for (String s2 : str2) {
+                        String el = "(" + s1 + ")" + s2;
+                        temp.add(el);
+                    }
+                }
+            }
+            result.add(temp);
+        }
+        return result.get(n);
+    }
 }
