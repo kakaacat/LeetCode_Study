85e51c97c3968292fb135c27891c70cf7a6d1daa
kakaacat <1912445179@qq.com>
2022-09-27 22:35:56 +0800

    反转链表

---
+++ LeetCode_cpp/LeeCode/LeeCode/reverse_linkedList.cpp
@@ -0,0 +1,47 @@
+﻿/**
+* 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+**/
+#include <iostream>
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+/**
+* \brief 迭代
+* \param[in] head：链表头结点指针
+*/
+ListNode* reverseList(ListNode* head)
+{
+	ListNode* prev = nullptr;
+	ListNode* curr = head;
+	while (curr != nullptr)
+	{
+		ListNode* next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	return curr;
+}
+
+/**
+* \brief 递归
+* \param[in] head：链表头结点指针
+*/
+ListNode* reverseList(ListNode* head)
+{
+	if (head == nullptr || head->next == nullptr)
+	{
+		return head;
+	}
+	ListNode* newHead = reverseList(head->next);
+	head->next->next = head;
+	head->next = nullptr;
+	return newHead;
+}
+
