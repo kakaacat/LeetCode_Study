fbfaafc7487c4ddf6bbeb04f957610622eba4ce4
kakaacat <1912445179@qq.com>
2022-10-04 20:31:34 +0800

    最长无重复字符串

---
+++ LeetCode_cpp/LeeCode/LeeCode/longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+/*
+* \brief 给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度。
+* \param s 字符串
+*/
+int LongestSubstring(string s)
+{
+	//集合set，记录每个字符是否出现过
+	unordered_set<char> set;
+
+	int rk = -1, ans = 0;
+
+	int n = s.size();
+
+	for (int i = 0; i < n; i++)
+	{
+		//左指针右移
+		if (i != 0)
+		{
+			set.erase(s[i - 1]);
+		}
+
+		while (rk + 1 != n && !set.count(s[rk + 1]))
+		{
+			set.insert(s[rk + 1]);
+			rk++;
+		}
+
+		ans = max(ans, rk - i + 1);
+	}
+	
+
+	return ans;
+}
