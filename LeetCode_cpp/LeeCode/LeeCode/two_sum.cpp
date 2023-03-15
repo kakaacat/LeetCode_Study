eda3ccdfafaacf91c70626e756bf682c7a4201c3
kakaacat <1912445179@qq.com>
2023-03-15 21:53:08 +0800

    two_sum

--- LeetCode_cpp/LeeCode/LeeCode/two_sum.cpp
+++ LeetCode_cpp/LeeCode/LeeCode/two_sum.cpp
@@ -15,12 +15,35 @@ public:
 		std::unordered_map<int, int> map;
 		for (int i = 0; i < nums.size(); i++)
 		{
-			int a = target - nums[i];
-			auto it = map.find(a);
+			auto it = map.find(target - nums[i]);
 			if (it != map.end())
 			{
 				return {i, it->second};
 			}
+			map.insert({ nums[i], i });
 		}
+		return {};
+	}
+
+
+	/// <summary>
+	/// ±©Á¦Ã¶¾Ù
+	/// </summary>
+	/// <param name="nums"></param>
+	/// <param name="target"></param>
+	/// <returns></returns>
+	std::vector<int> twoSum_violence(std::vector<int>& nums, int target)
+	{
+		for (int i = 0; i < nums.size(); i++)
+		{
+			for (int j = i + 1; j < nums.size(); j++)
+			{
+				if (nums[i] + nums[j] == target)
+				{
+					return { i, j };
+				}
+			}
+		}
+		return {};
 	}
 };
