5150fe8bb41c9ca8f70f92cdc37f35549895b900
kakaacat <1912445179@qq.com>
2023-08-28 21:43:35 +0800

    [add]

--- LeetCode_cpp/LeeCode/LeeCode/search_in_rotated_sorted_array.cpp
+++ LeetCode_cpp/LeeCode/LeeCode/search_in_rotated_sorted_array.cpp
@@ -12,46 +12,26 @@ public:
 	/// <returns></returns>
 	int search(std::vector<int>& nums, int target)
 	{
-		int n = nums.size();
-		if (!n)
-		{
-			return -1;
-		}
-		if (n == 1)
-		{
-			return (target == nums[0] ? 0 : -1);
-		}
-		int l = 0, r = n - 1;
-		while (l <= r)
-		{
-			int mid = (l + r) / 2;
-			if (nums[mid] == target)
-			{
-				return (mid);
-			}
-			//×ó²àÓĞĞò
-			if (nums[0] <= nums[mid])
-			{
-				if (nums[0] <= target && target < nums[mid])
-				{
-					r = mid - 1;
-				}
-				else {
-					l = mid + 1;
-				}
-			}
-			//ÓÒ²àÓĞĞò
-			else {
-				if (nums[mid] < target && target <= nums[n-1])
-				{
-					l = mid + 1;
-				}
-				else {
-					r = mid - 1;
-				}
-			}
-		}
-		return (-1);
+        int n = nums.size();
+        if (n == 0) return -1;
+        if (n == 1) return (nums[0] == target ? 0 : -1);
+
+        int l = 0, r = n - 1;
+        while (l <= r)
+        {
+            int mid = (r - l) / 2 + l;
+
+            if (nums[mid] == target) return mid;
+
+            if (nums[mid] < target)
+            {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return -1;
 
 	}
 };
