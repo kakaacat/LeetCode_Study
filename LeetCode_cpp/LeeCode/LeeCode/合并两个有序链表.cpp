ed8397df11271397179bd22cbf096432fa0dcb77
kakaacat <1912445179@qq.com>
2023-08-28 21:52:31 +0800

    [add] 合并两个链表-迭代

--- LeetCode_cpp/LeeCode/LeeCode/合并两个有序链表.cpp
+++ LeetCode_cpp/LeeCode/LeeCode/合并两个有序链表.cpp
@@ -11,6 +11,12 @@ struct ListNode
 	ListNode(int val, ListNode* n) : value(val), next(n) {}
 };
 
+/// <summary>
+/// µÝ¹é
+/// </summary>
+/// <param name="l1"></param>
+/// <param name="l2"></param>
+/// <returns></returns>
 ListNode* merge(ListNode* l1, ListNode* l2)
 {
 	//ListNode* preHead = new ListNode(-1, nullptr);
@@ -39,6 +45,29 @@ ListNode* merge(ListNode* l1, ListNode* l2)
 	return preHead->next;
 }
 
+//µü´ú
+ListNode* merge2(ListNode* l1, ListNode* l2)
+{
+	if (l1 == nullptr)
+	{
+		return l2;
+	}
+	if (l2 == nullptr)
+	{
+		return l1;
+	}
+
+	if (l1->value < l2->value)
+	{
+		l1->next = merge2(l1->next, l2);
+		return l1;
+	}
+	else
+	{
+		l2->next = merge2(l1, l2->next);
+		return l2;
+	}
+}
 
 int main()
 {
